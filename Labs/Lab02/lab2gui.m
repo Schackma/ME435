64766023479c94db372101c9325a63a9eba49976
Matthew <schackma@rose-hulman.edu>
2016-03-16 21:58:00 -0400

    so fucking pro

--- Labs/Lab02/lab2gui.m
+++ Labs/Lab02/lab2gui.m
@@ -1,3 +1,108 @@
+function varargout = lab2gui(varargin)
+% LAB2GUI MATLAB code for lab2gui.fig
+%      LAB2GUI, by itself, creates a new LAB2GUI or raises the existing
+%      singleton*.
+%
+%      H = LAB2GUI returns the handle to a new LAB2GUI or the handle to
+%      the existing singleton*.
+%
+%      LAB2GUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in LAB2GUI.M with the given input arguments.
+%
+%      LAB2GUI('Property','Value',...) creates a new LAB2GUI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before lab2gui_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to lab2gui_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help lab2gui
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @lab2gui_OpeningFcn, ...
+                   'gui_OutputFcn',  @lab2gui_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% —- Executes just before lab2gui is made visible.
+function lab2gui_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to lab2gui (see VARARGIN)
+
+% Choose default command line output for lab2gui
+clc
+handles.output = hObject;
+handles.user.connected = false;
+handles = resetParams(handles);
+
+handles = massDisable(handles);
+
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes lab2gui wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% —- Outputs from this function are returned to the command line.
+function varargout = lab2gui_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% —- Executes on button press in pushbutton_reset.
+function pushbutton_reset_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton_reset (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+set(handles.text_status,'String',handles.user.robot.reset);
+handles = resetParams(handles);
+guidata(hObject,handles);
+
+
+% —- Executes on button press in pushbutton_toggleZ.
+function pushbutton_toggleZ_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton_toggleZ (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+if(handles.user.extended)
+    set(hObject,'String','Extend');
+    set(handles.text_status,'String',handles.user.robot.retract);
+else
+    set(hObject,'String','Retract');
+    set(handles.text_status,'String',handles.user.robot.extend);
+end
+handles.user.extended = ~handles.user.extended;
+guidata(hObject,handles);
+
+
 % —- Executes on button press in pushbutton_toggleGrip.
 function pushbutton_toggleGrip_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton_toggleGrip (see GCBO)
