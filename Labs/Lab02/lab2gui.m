55bc7221a9b015634bb4906eead81cfdd7590877
GunnarHorve <horvegc@rose-hulman.edu>
2016-03-16 20:56:25 -0400

    merged schack's work with mine.

--- Labs/Lab02/lab2gui.m
+++ Labs/Lab02/lab2gui.m
@@ -22,7 +22,7 @@ function varargout = lab2gui(varargin)
 
 % Edit the above text to modify the response to help lab2gui
 
-% Last Modified by GUIDE v2.5 16-Mar-2016 20:33:08
+% Last Modified by GUIDE v2.5 16-Mar-2016 20:55:54
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -44,7 +44,7 @@ end
 % End initialization code - DO NOT EDIT
 
 
-% --- Executes just before lab2gui is made visible.
+% —- Executes just before lab2gui is made visible.
 function lab2gui_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
@@ -54,6 +54,9 @@ function lab2gui_OpeningFcn(hObject, eventdata, handles, varargin)
 
 % Choose default command line output for lab2gui
 handles.output = hObject;
+handles.user.connected = false;
+resetParams(handles);
+
 
 % Update handles structure
 guidata(hObject, handles);
@@ -62,7 +65,7 @@ guidata(hObject, handles);
 % uiwait(handles.figure1);
 
 
-% --- Outputs from this function are returned to the command line.
+% —- Outputs from this function are returned to the command line.
 function varargout = lab2gui_OutputFcn(hObject, eventdata, handles) 
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
@@ -73,28 +76,32 @@ function varargout = lab2gui_OutputFcn(hObject, eventdata, handles)
 varargout{1} = handles.output;
 
 
-% --- Executes on button press in pushbutton_reset.
+% —- Executes on button press in pushbutton_reset.
 function pushbutton_reset_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton_reset (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+set(handles.edit_response,'String',handles.user.robot.reset);
+resetparams;
 
 
-% --- Executes on button press in pushbutton_toggleZ.
+% —- Executes on button press in pushbutton_toggleZ.
 function pushbutton_toggleZ_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton_toggleZ (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+set(handles.edit_response,'String',handles.user.robot,extend);
 
 
-% --- Executes on button press in pushbutton_toggleGrip.
+
+
+% —- Executes on button press in pushbutton_toggleGrip.
 function pushbutton_toggleGrip_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton_toggleGrip (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-
-
+Matthew Schack, [16.03.16 20:55]
 function edit_comPort_Callback(hObject, eventdata, handles)
 % hObject    handle to edit_comPort (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -104,7 +111,7 @@ function edit_comPort_Callback(hObject, eventdata, handles)
 %        str2double(get(hObject,'String')) returns contents of edit_comPort as a double
 
 
-% --- Executes during object creation, after setting all properties.
+% —- Executes during object creation, after setting all properties.
 function edit_comPort_CreateFcn(hObject, eventdata, handles)
 % hObject    handle to edit_comPort (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -117,10 +124,70 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
 end
 
 
-% --- Executes on button press in checkbox_connect.
+% —- Executes on button press in checkbox_connect.
 function checkbox_connect_Callback(hObject, eventdata, handles)
 % hObject    handle to checkbox_connect (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
 % Hint: get(hObject,'Value') returns toggle state of checkbox_connect
+
+
+% —- Executes when selected object is changed in uibuttongroup_xaxis.
+function uibuttongroup_xaxis_SelectionChangedFcn(hObject, eventdata, handles)
+% hObject    handle to the selected object in uibuttongroup_xaxis 
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+
+function edit_moveFrom_Callback(hObject, eventdata, handles)
+% hObject    handle to edit_moveFrom (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit_moveFrom as text
+%        str2double(get(hObject,'String')) returns contents of edit_moveFrom as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit_moveFrom_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit_moveFrom (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton_move.
+function pushbutton_move_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton_move (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+
+function edit_moveTo_Callback(hObject, eventdata, handles)
+% hObject    handle to edit_moveTo (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit_moveTo as text
+%        str2double(get(hObject,'String')) returns contents of edit_moveTo as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit_moveTo_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit_moveTo (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
