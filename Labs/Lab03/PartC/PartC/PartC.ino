d9f9a9a1d4c5449e86a082107b7774f44f431830
GunnarHorve <horvegc@rose-hulman.edu>
2016-03-30 13:06:45 -0400

    added Lab3, partC (blank)

---
+++ Labs/Lab03/PartC/PartC/PartC.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		PartC.ino
+ Created:	3/30/2016 1:06:07 PM
+ Author:	horvegc
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
