eb73a22f1618dbb41484128cbd86ace463c89ea0
Matthew <schackma@rose-hulman.edu>
2016-04-12 13:26:55 -0400

    golf ball things

--- Matthew_stuff/Classwork/golf_ball_stand_test/golf_ball_stand_test/golf_ball_stand_test.ino
+++ Matthew_stuff/Classwork/golf_ball_stand_test/golf_ball_stand_test/golf_ball_stand_test.ino
@@ -4,12 +4,20 @@
  Author:	schackma
 */
 
+#include <GolfBallStand.h>
+
+GolfBallStand stand;
+
 // the setup function runs once when you press reset or power the board
 void setup() {
-
+	Serial.begin(9600);
+	stand.setLedState(LED_RED, LOCATION_3, LED_FRONT);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
+
+	stand.determineBallColor(LOCATION_1);
+	delay(1000);
   
 }
