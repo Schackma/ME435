b5c6d8844a1334a8dd4c58f8b8fe679ba259e797
Matthew <schackma@rose-hulman.edu>
2016-04-12 12:48:44 -0400

    golf ball stand code

---
+++ Matthew_stuff/Classwork/golf_ball_stand_test/golf_ball_stand_test/golf_ball_stand_test.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		golf_ball_stand_test.ino
+ Created:	4/12/2016 12:47:07 PM
+ Author:	schackma
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
