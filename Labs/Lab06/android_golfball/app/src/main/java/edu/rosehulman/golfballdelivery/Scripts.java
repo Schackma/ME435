2431b8debe7f801e722c630498aaaa3d4e0a1045
Matthew <schackma@rose-hulman.edu>
2016-05-04 18:02:45 -0400

    things and stuff

--- Labs/Lab06/android_golfball/app/src/main/java/edu/rosehulman/golfballdelivery/Scripts.java
+++ Labs/Lab06/android_golfball/app/src/main/java/edu/rosehulman/golfballdelivery/Scripts.java
@@ -20,14 +20,15 @@ public class Scripts {
     private int RIGHT = 3;
 
     private int home[] = {0,90,0,-90,90};
-    private int restPos[] = {0,0,0,0,0};
 
-    private int killRight[] = {-19,119,-90,-147,96};
-    private int killMid[] = {6,119,-90,-147,96};
-    private int killLeft[] = {39,119,-90,-147,96};
+    private int killRight[] = {-19,125,-90,-147,96};
+    private int killMid[] = {8,120,-81,-163,96};
+    private int killLeft[] = {39,122,-90,-147,96};
 
-    private int boop[] = { -43,119,-90,-147,96 };
-    private int boopLeft[] = {79,119,-90,-147,96};
+    private int boop[] = { -43,125, 90,-147,96 };
+    private int boopMid[] ={8,150,-81,-90,-147,60};
+    private int boopLeft[] = {79,122,-90,-147,96};
+
 
     /** Reference to the primary activity. */
     private GolfBallDeliveryActivity mGolfBallDeliveryActivity;
@@ -36,7 +37,7 @@ public class Scripts {
     protected Handler mCommandHandler = new Handler();
 
     /** Time in milliseconds needed to perform a ball removal. */
-    private int ARM_REMOVAL_TIME_MS = 3000;
+    private int ARM_REMOVAL_TIME_MS = 8000;
 
     /** Simple constructor. */
     public Scripts(GolfBallDeliveryActivity golfBallDeliveryActivity) {
@@ -62,54 +63,23 @@ public class Scripts {
 
     /** Runs the script to drive to the near ball (perfectly straight) and drop it off. */
     public void nearBallScript() {
-        Toast.makeText(mGolfBallDeliveryActivity, "Drive 103 ft to near ball.", Toast.LENGTH_SHORT).show();
-        double distanceToNearBall = NavUtils.getDistance(15, 0, 90, 50);
-        long driveTimeToNearBallMs = (long) (distanceToNearBall / RobotActivity.DEFAULT_SPEED_FT_PER_SEC * 1000);
-        driveTimeToNearBallMs = 3000; // Make this mock script not take so long.
-        mGolfBallDeliveryActivity.sendWheelSpeed(mGolfBallDeliveryActivity.mLeftStraightPwmValue, mGolfBallDeliveryActivity.mRightStraightPwmValue);
-        mCommandHandler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                removeBallAtLocation(mGolfBallDeliveryActivity.mNearBallLocation);
-            }
-        }, driveTimeToNearBallMs);
-        mCommandHandler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                if (mGolfBallDeliveryActivity.mState == GolfBallDeliveryActivity.State.NEAR_BALL_SCRIPT) {
-                    mGolfBallDeliveryActivity.setState(GolfBallDeliveryActivity.State.FAR_BALL_SCRIPT);
-                }
-            }
-        }, driveTimeToNearBallMs + ARM_REMOVAL_TIME_MS);
+        removeBallAtLocation(mGolfBallDeliveryActivity.mNearBallLocation,GolfBallDeliveryActivity.State.GO_TO_MID);
     }
 
 
     /** Script to drop off the far ball. */
     public void farBallScript() {
-        mGolfBallDeliveryActivity.sendWheelSpeed(0, 0);
-        Toast.makeText(mGolfBallDeliveryActivity, "Figure out which ball(s) to remove and do it.", Toast.LENGTH_SHORT).show();
-        removeBallAtLocation(mGolfBallDeliveryActivity.mFarBallLocation);
-        mCommandHandler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                if (mGolfBallDeliveryActivity.mWhiteBallLocation != 0) {
-                    removeBallAtLocation(mGolfBallDeliveryActivity.mWhiteBallLocation);
-                }
-                if (mGolfBallDeliveryActivity.mState == GolfBallDeliveryActivity.State.FAR_BALL_SCRIPT) {
-                    mGolfBallDeliveryActivity.setState(GolfBallDeliveryActivity.State.DRIVE_TOWARDS_HOME);
-                }
-            }
-        }, ARM_REMOVAL_TIME_MS);
+        removeBallAtLocation(mGolfBallDeliveryActivity.mFarBallLocation, GolfBallDeliveryActivity.State.DRIVE_TOWARDS_HOME);
     }
 
 
     // -------------------------------- Arm script(s) ----------------------------------------
 
     /** Removes a ball from the golf ball stand. */
-    public void removeBallAtLocation(final int location) {
+    public void removeBallAtLocation(final int location,final GolfBallDeliveryActivity.State newState) {
 
         mGolfBallDeliveryActivity.sendCommand("ATTACH 111111"); // Just in case
-        mGolfBallDeliveryActivity.sendCommand("GRIPPER 50");    // Just in case
+        mGolfBallDeliveryActivity.sendCommand("GRIPPER 55");    // Just in case
 
         mCommandHandler.postDelayed(new Runnable() {
             @Override
@@ -117,15 +87,17 @@ public class Scripts {
                 sendPos(home);
             }
         }, 10);
+
+
         mCommandHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 if(location == RIGHT) {
                     sendPos(killRight);
-                } else if (location == CENTER) {
-                    sendPos(killMid);
-                } else if (location == LEFT) {
+                } else if(location == LEFT){
                     sendPos(killLeft);
+                }else{
+                    sendPos(killMid);
                 }
             }
         }, 2000);
@@ -134,8 +106,10 @@ public class Scripts {
             public void run() {
                 if(location == LEFT) {
                     sendPos(boopLeft);
-                } else {
+                } else if (location == RIGHT){
                     sendPos(boop);
+                }else{
+                    sendPos(boopMid);
                 }
                 mGolfBallDeliveryActivity.setLocationToColor(location, GolfBallDeliveryActivity.BallColor.NONE);
             }
@@ -143,20 +117,81 @@ public class Scripts {
         mCommandHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
-//                sendPos(restPos);
                 sendPos(home);
+                mGolfBallDeliveryActivity.setState(newState);
             }
         }, 5000);
-        mCommandHandler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                sendPos(home);
-                //actually change state
-            }
-        }, 7000);
     }
 
     private void sendPos(int toSend[]) {
         mGolfBallDeliveryActivity.sendCommand(String.format("POSITION %d %d %d %d %d",toSend[0], toSend[1], toSend[2], toSend[3], toSend[4]));
+    }
+
+    public void goToNearBallScript() {
+        double distanceToNearBall = NavUtils.getDistance(15, 0, 90, 50);
+        long driveTimeToNearBallMs = (long) (distanceToNearBall / RobotActivity.DEFAULT_SPEED_FT_PER_SEC * 1000);
+        driveTimeToNearBallMs = 0; // Make this mock script not take so long.
+        mGolfBallDeliveryActivity.sendWheelSpeed(mGolfBallDeliveryActivity.mLeftStraightPwmValue, mGolfBallDeliveryActivity.mRightStraightPwmValue);
+        mCommandHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mGolfBallDeliveryActivity.sendWheelSpeed(0, 0);
+                mGolfBallDeliveryActivity.setState(GolfBallDeliveryActivity.State.NEAR_BALL_SCRIPT);
+            }
+        }, driveTimeToNearBallMs);
+
+    }
+
+    public void goToMidScript() {
+        double distanceToMidBall = NavUtils.getDistance(15, 0, 90, 50);
+        long driveTimeToMidBallMs = (long) (distanceToMidBall / RobotActivity.DEFAULT_SPEED_FT_PER_SEC * 1000);
+        driveTimeToMidBallMs = 1000; // Make this mock script not take so long.
+        mGolfBallDeliveryActivity.sendWheelSpeed(mGolfBallDeliveryActivity.mLeftStraightPwmValue, mGolfBallDeliveryActivity.mRightStraightPwmValue);
+        mCommandHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mGolfBallDeliveryActivity.sendWheelSpeed(0, 0);
+                mGolfBallDeliveryActivity.setState(GolfBallDeliveryActivity.State.MID_BALL_SCRIPT);
+            }
+        }, driveTimeToMidBallMs);
+
+    }
+
+    public void midBallScript() {
+        if (mGolfBallDeliveryActivity.mWhiteBallLocation != 0) {
+            removeBallAtLocation(mGolfBallDeliveryActivity.mWhiteBallLocation, GolfBallDeliveryActivity.State.GO_TO_FAR_BALL);
+        }else {
+            mGolfBallDeliveryActivity.setState(GolfBallDeliveryActivity.State.GO_TO_FAR_BALL);
+        }
+    }
+
+    public void goToFarBallScript() {
+        double distanceToFarBall = NavUtils.getDistance(15, 0, 90, 50);
+        long driveTimeToFarBallMs = (long) (distanceToFarBall / RobotActivity.DEFAULT_SPEED_FT_PER_SEC * 1000);
+        driveTimeToFarBallMs = 1000; // Make this mock script not take so long.
+        mGolfBallDeliveryActivity.sendWheelSpeed(mGolfBallDeliveryActivity.mLeftStraightPwmValue, mGolfBallDeliveryActivity.mRightStraightPwmValue);
+        mCommandHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mGolfBallDeliveryActivity.sendWheelSpeed(0, 0);
+                mGolfBallDeliveryActivity.setState(GolfBallDeliveryActivity.State.FAR_BALL_SCRIPT);
+            }
+        }, driveTimeToFarBallMs);
+
+    }
+
+    public void driveTowardsHomeScript() {
+        double distanceToHome = NavUtils.getDistance(15, 0, 90, 50);
+        long driveTimeToHomeMs = (long) (distanceToHome / RobotActivity.DEFAULT_SPEED_FT_PER_SEC * 1000);
+        driveTimeToHomeMs = 2000; // Make this mock script not take so long.
+        mGolfBallDeliveryActivity.sendWheelSpeed(-mGolfBallDeliveryActivity.mLeftStraightPwmValue, -mGolfBallDeliveryActivity.mRightStraightPwmValue);
+        Toast.makeText(mGolfBallDeliveryActivity,"stupid ass bitch",Toast.LENGTH_SHORT).show();
+        mCommandHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(mGolfBallDeliveryActivity,"fuck this",Toast.LENGTH_SHORT).show();
+                mGolfBallDeliveryActivity.sendWheelSpeed(0, 0);
+                mGolfBallDeliveryActivity.setState(GolfBallDeliveryActivity.State.READY_FOR_MISSION);            }
+        }, driveTimeToHomeMs);
     }
 }
