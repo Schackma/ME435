9def8aff48d376d8de9c077587d4c69264247fa2
Matthew <schackma@rose-hulman.edu>
2016-05-06 12:19:24 -0400

    my things

---
+++ Matthew_stuff/Classwork/Integrated_Image_Req/app/src/main/java/schackma/integrated_image_req/MainActivity.java
@@ -0,0 +1,55 @@
+package schackma.integrated_image_req;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
+
+import golfballdelivery.GolfBallDeliveryActivity;
+
+public class MainActivity extends GolfBallDeliveryActivity {
+
+    private ViewFlipper mViewFlipper;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mViewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_next) {
+            mViewFlipper.showNext();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
